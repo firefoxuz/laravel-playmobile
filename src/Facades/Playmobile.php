2c208e357d9264509a74db5ec1127c74c3c080ed
Sultonov Daler <daler.sultonov.00@gmail.com>
2022-05-01 20:46:22 +0500

    added playmobile facade

---
+++ src/Facades/Playmobile.php
@@ -0,0 +1,15 @@
+<?php
+namespace Firefoxuz\LaravelPlaymobile\Facades;
+
+class Playmobile extends \Illuminate\Support\Facades\Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return \Firefoxuz\LaravelPlaymobile\Playmobile::class;
+    }
+}
